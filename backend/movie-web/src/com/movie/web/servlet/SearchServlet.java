b676e04320faa6624aa9a857becf72ca9b4e3103
suxss <82267359+suxss@users.noreply.github.com>
2022-12-01 17:24:28 +0000

    update 2 servlet visit path

--- backend/movie-web/src/com/movie/web/servlet/SearchServlet.java
+++ backend/movie-web/src/com/movie/web/servlet/SearchServlet.java
@@ -6,7 +6,7 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet(name = "SearchServlet", value = "/SearchServlet")
+@WebServlet(name = "SearchServlet", value = "/search")
 public class SearchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
